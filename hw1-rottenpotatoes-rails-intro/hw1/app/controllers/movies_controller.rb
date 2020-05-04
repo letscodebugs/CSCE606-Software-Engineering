0c7742a9390184ec41c3b8b33d40f5997336bff4
tyu hwuang <tyu.hwuang@gmail.com>
2020-05-03 20:01:07 -0500

    change structure of repo

---
+++ hw1-rottenpotatoes-rails-intro/hw1/app/controllers/movies_controller.rb
@@ -0,0 +1,60 @@
+class MoviesController < ApplicationController
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
+
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.<extension> by default
+  end
+
+  def index
+    if params.key?(:sort_by)
+      session[:sort_by] = params[:sort_by]
+    elsif session.key?(:sort_by)
+      params[:sort_by] = session[:sort_by]
+      redirect_to movies_path(params) and return
+    end
+    @hilite = sort_by = session[:sort_by]
+    @all_ratings = Movie.all_ratings
+    if params.key?(:ratings)
+      session[:ratings] = params[:ratings]
+    elsif session.key?(:ratings)
+      params[:ratings] = session[:ratings]
+      redirect_to movies_path(params) and return
+    end
+    @checked_ratings = (session[:ratings].keys if session.key?(:ratings)) || @all_ratings
+    @movies = Movie.order(sort_by).where(rating: @checked_ratings)
+  end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    @movie = Movie.create!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+
+end
---
+++ hw1-rottenpotatoes-rails-intro/hw1/app/models/movie.rb
@@ -0,0 +1,6 @@
+class Movie < ActiveRecord::Base
+  @@all_ratings = ['G','PG','PG-13','R']
+  def self.all_ratings
+    @@all_ratings
+  end
+end
